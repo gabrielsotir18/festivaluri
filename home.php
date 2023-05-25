6fdc12bef0b0c26c68cfbd6015271da74b94b813
gabrielsotir18 <gabrielsotir18@yahoo.com>
2023-05-25 20:42:38 +0300

    dasdaw

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// session_start();
+session_start();
 include "db_connect.php";
 ?>
 <title>W3.CSS Template</title>
