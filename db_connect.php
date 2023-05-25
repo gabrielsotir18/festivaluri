c5e3b30e25eadef431787b381a5f724bc267c6c3
gabrielsotir18 <gabrielsotir18@yahoo.com>
2023-05-25 19:24:42 +0300

    wdaPlease enter the commit message for your changes. Lines starting

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 
-$con = mysqli_init();
-mysqli_options($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
-mysqli_real_connect($conn, "ebusiness.mysql.database.azure.com", "gabosman", "Capdecal-70", "Capdecal-70", 3306, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT);
+$mysqli = mysqli_init();
+mysqli_options($mysqli, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+mysqli_real_connect($mysqli, "ebusiness.mysql.database.azure.com", "gabosman", "Capdecal-70", "Capdecal-70", 3306, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT);
 
 ?>
