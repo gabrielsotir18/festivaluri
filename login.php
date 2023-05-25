4aeb5d761a69b300ae79383365d2fca610f4928f
gabrielsotir18 <gabrielsotir18@yahoo.com>
2023-05-25 20:00:40 +0300

    dasdas

--- login.php
+++ login.php
@@ -25,8 +25,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 		$result = $mysqli->query($sql);
 
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
+		if ($row = $result->fetch_assoc()) {
             if ($row['user_name'] === $uname && $row['password'] === $pass) {
             	$_SESSION['user_name'] = $row['user_name'];
             	$_SESSION['name'] = $row['name'];
