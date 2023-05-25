250f62524c69230fd529b2969db5ebcb73ef4e99
gabrielsotir18 <gabrielsotir18@yahoo.com>
2023-05-25 21:17:19 +0300

    dasd

--- login.php
+++ login.php
@@ -34,8 +34,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
             	$_SESSION['user_name'] = $row['user_name'];
             	$_SESSION['name'] = $row['name'];
             	$_SESSION['id'] = $row['id'];
-            	header("Location: home.php");
-		        exit();
+            	echo '<meta http-equiv="refresh" content="0; URL=home.php">';
             }else{
 				header("Location: LoginPage.php?error=Incorect User name or password");
 		        exit();
