800afe13e13c149748b45f20daf5def80f30cb1d
gabrielsotir18 <gabrielsotir18@yahoo.com>
2023-05-25 19:43:14 +0300

    connection

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_init();
+$mysqli = mysqli_init();
 mysqli_options($mysqli, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 mysqli_real_connect($mysqli, "ebusiness.mysql.database.azure.com", "gabosman", "Capdecal-70", "festivalDB", 3306, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT);
 
